2be51d299758114fd1729986111e84195bd57617
Jim Coven <jattcode@gmail.com>
2015-07-22 17:39:17 +0800

    wip

--- mod_sample/src/main/java/com.jattcode.android/auori.demo/MultiViewActivity.java
+++ mod_sample/src/main/java/com.jattcode.android/auori.demo/MultiViewActivity.java
@@ -1,19 +1,33 @@
+/*
+ *
+ *  * Copyright (c) 2015. JattCode.com
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ *
+ *
+ */
+
 package com.jattcode.android.auori.demo;
 
 import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.jattcode.android.auori.view.AuoriCropImageView;
@@ -21,7 +35,7 @@ import com.jattcode.android.auori.view.AuoriCropImageView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MultiViewActivity extends AppCompatActivity {
 
     private static class Configuration {
         final int scaleCropType;
